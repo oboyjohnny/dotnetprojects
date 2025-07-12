51f81b90b7d4e2a950d15d2e1be75bd26c931e25
oboyjohnny <oboyjohnny@gmail.com>
2025-07-12 19:45:18 +0100

    Todo conroller and view added

---
+++ TodoListApp/Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoListApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+    }
+}
